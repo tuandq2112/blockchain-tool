b96c7bf374985ff9d297a142d6ac8e87faa358cf
tuan.dq <doquoctuan311@gmail.com>
2022-12-23 08:48:17 +0700

    #optimize code

--- Layout/index.js
+++ Layout/index.js
@@ -11,6 +11,20 @@ export default function CustomLayout({ children }) {
       <Head>
         <title>Welcome Naut's blogs</title>
         <link rel="shortcut icon" href="/logohead.png" />
+        <meta charset="UTF-8" />
+        <meta name="description" content="Free some web3 tools!" />
+        <meta name="keywords" content="web3, tools,..." />
+        <meta name="author" content="tuandq" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        {/* <meta property="og:url" content="https://nothing.com" /> */}
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="Web3 website" />
+        <meta property="og:description" content="Free some web3 tools!" />
+        <meta
+          property="og:image"
+          content="https://d3lkc3n5th01x7.cloudfront.net/wp-content/uploads/2022/07/18031856/Web3-Vs-Web-3.0.png"
+        />
       </Head>
       <Header />
       <Content>{children} </Content>

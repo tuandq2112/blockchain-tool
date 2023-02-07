21e9a35fc01acc05ec946404f5a1b6b9a1bdcfcc
tuan.dq <doquoctuan311@gmail.com>
2023-02-07 10:53:28 +0700

    #Add head

--- Layout/index.js
+++ Layout/index.js
@@ -1,5 +1,4 @@
 import "antd/dist/reset.css";
-import Head from "next/head";
 import Content from "./Content";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -8,24 +7,6 @@ import { RootWrapper } from "./styled";
 export default function CustomLayout({ children }) {
   return (
     <RootWrapper>
-      <Head>
-        <title>Welcome Naut's blogs</title>
-        <link rel="shortcut icon" href="/logohead.png" />
-        <meta charset="UTF-8" />
-        <meta name="description" content="Free some web3 tools!" />
-        <meta name="keywords" content="web3, tools,..." />
-        <meta name="author" content="tuandq" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-        {/* <meta property="og:url" content="https://nothing.com" /> */}
-        <meta property="og:type" content="article" />
-        <meta property="og:title" content="Web3 website" />
-        <meta property="og:description" content="Free some web3 tools!" />
-        <meta
-          property="og:image"
-          content="https://d3lkc3n5th01x7.cloudfront.net/wp-content/uploads/2022/07/18031856/Web3-Vs-Web-3.0.png"
-        />
-      </Head>
       <Header />
       <Content>{children} </Content>
       <Footer />
--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,7 @@ import { useEffect, useState } from "react";
 import { GlobalStyled } from "styles/styled";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
 import { bsc, bscTestnet } from "wagmi/chains";
+import Head from "next/head";
 
 // 1. Get projectID at https://cloud.walletconnect.com
 if (!process.env.NEXT_PUBLIC_PROJECT_ID) {
@@ -54,6 +55,24 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <title>Welcome Naut's blogs</title>
+        <link rel="shortcut icon" href="/logohead.png" />
+        <meta charset="UTF-8" />
+        <meta name="description" content="Free some web3 tools!" />
+        <meta name="keywords" content="web3, tools,..." />
+        <meta name="author" content="tuandq" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        {/* <meta property="og:url" content="https://nothing.com" /> */}
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="Web3 website" />
+        <meta property="og:description" content="Free some web3 tools!" />
+        <meta
+          property="og:image"
+          content="https://d3lkc3n5th01x7.cloudfront.net/wp-content/uploads/2022/07/18031856/Web3-Vs-Web-3.0.png"
+        />
+      </Head>
       <GlobalStyled />
       {ready ? (
         <WagmiConfig client={wagmiClient}>

17ba0306623ec426ba9674848ec0849195f1a5c0
tuan.dq <doquoctuan311@gmail.com>
2023-07-26 17:20:28 +0700

    #Fix ivirse

--- pages/_app.js
+++ pages/_app.js
@@ -40,8 +40,9 @@ function App({ Component, pageProps }) {
             projectId={PROJECT_ID}
             ethereumClient={ethereumClient}
             explorerRecommendedWalletIds={[
-              "4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0",
               "060da523cddd8e2ce9c60dfff883f293b911c8e7e17ec8c70174069e9b5e716d",
+              "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96",
+              "4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0",
               "1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369",
             ]}
           />
